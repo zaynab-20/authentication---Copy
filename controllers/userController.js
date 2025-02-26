138bc9177191abbacb64683cf1689398b4e4ddde
zaynab-20 <zatoloye@gmail.com>
2025-02-26 01:06:22 +0100

    c

--- controllers/userController.js
+++ controllers/userController.js
@@ -318,13 +318,31 @@ exports.changePassword = async (req, res) =>{
     }
 }
 
-// exports.isAdmin = async (req, res) =>{
-//     try {
+exports.isAdmin = async (req, res) =>{
+    try {
+        const {id} = req.params
+        const user = await userModel.findById(id);
         
-//     } catch (error) {
-//         console.log(error.message)        
-//         res.status(500).json({
-//             message: 'internal server error'
-//         })
-//     }
-// }
+        if (!user) {
+            return res.status(404).json({
+                message: 'user not found'
+            })
+        }
+        user.isAdmin = true
+        if (!req.user.isAdmin) {
+            return res.status(403).json({
+                message: 'you are not authorized to make this change'
+
+            })
+        }
+
+        res.status(200).json({
+            message: `user are now an admin`
+        })
+    } catch (error) {
+        console.log(error.message)        
+        res.status(500).json({
+            message: 'internal server error'
+        })
+    }
+}
--- middleWare/authentication.js
+++ middleWare/authentication.js
@@ -91,3 +91,39 @@ const jwt = require('jsonwebtoken')
         })
     }
  }
+
+ exports.authenticateToAdmin = async (req, res, next) =>{
+    try {
+        const auth = req.headers.authorization
+        if (!auth) {
+            return res.status(404).json({
+                message: 'token not found'
+            })
+        }
+        const token = auth.split(' ')[1]
+        if (!token) {
+            return res.status(404).json({
+                message: 'invalid token'
+            })
+        }
+        const decodedToken = await jwt.verify(token, process.env.JWT_SECRET)
+        const user = await userModel.findById(decodedToken, userId)
+        if (!user) {
+            return res.status(400).json({
+                message: 'user not found'
+            })
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        console.log(error.message);
+        if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(400).json({
+                message: 'session time-out, please login to countinue'
+            })
+        }
+        res.status(500).json({
+            message: 'internal server error'
+        })
+    }
+ }
